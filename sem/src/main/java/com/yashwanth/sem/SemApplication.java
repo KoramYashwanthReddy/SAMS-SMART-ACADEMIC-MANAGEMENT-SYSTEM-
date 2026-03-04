03ea650468f55316c987c6df9b6804c6431dd605
Yashwanth Reddy Koram <koramyashwanthr@gmail.com>
2026-03-04 20:18:06 +0530

    Project Creation

---
+++ sem/src/main/java/com/yashwanth/sem/SemApplication.java
@@ -0,0 +1,15 @@
+package com.yashwanth.sem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+
+@SpringBootApplication
+public class SemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemApplication.class, args);
+	}
+
+}
